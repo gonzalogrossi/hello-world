b91e7b3904d46bba1a675371705fc436fa907765
gonzalogrossi <42222337+gonzalogrossi@users.noreply.github.com>
2018-08-31 20:16:44 -0300

    solucion de programa hello world

---
+++ PII_Expert_Ejercicio-master/Alfajor.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Expert_SRP
+{
+    public class Alfajor 
+    {
+        public Alfajor(double precioMasa, double precioDulce)
+        {
+            this.PrecioDulce = precioDulce;
+            this.PrecioMasa = precioMasa;
+        }
+        public Double PrecioDulce {get;set;}
+        public Double PrecioMasa {get; set;}
+        public double valor()
+    }
+}
